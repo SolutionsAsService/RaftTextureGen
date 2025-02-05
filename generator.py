1a7f1982ab85a7eb4a95261868d4d0d8c240ebda
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-02-05 15:49:47 -0800

    less lashing

--- generator.py
+++ generator.py
@@ -37,7 +37,7 @@ def generate_raft_image():
     draw = ImageDraw.Draw(image)
 
     # Define plank and lashing sizes
-    plank_width = (width - 20) // 5
+    plank_width = (width - 40) // 5
     plank_height = height - 40
 
     # Draw bamboo planks
@@ -46,15 +46,11 @@ def generate_raft_image():
         y = 20
         draw_bamboo_plank(draw, x, y, plank_width, plank_height)
 
-    # Draw horizontal lashings across the planks
-    for i in range(6):
-        x = 10
-        y = i * (plank_height // 5) + 20
-        draw_lashing(draw, x, y, width - 20, horizontal=True)
-
-    # Draw vertical lashings at the ends
-    draw_lashing(draw, 10 - 6, 20, plank_height, horizontal=False)
-    draw_lashing(draw, width - 10, 20, plank_height, horizontal=False)
+    # Draw vertical lashings barely holding the planks together
+    for i in range(1, 5):
+        x = i * plank_width + 7
+        y = 20
+        draw_lashing(draw, x, y, plank_height, horizontal=False)
 
     return image
 
