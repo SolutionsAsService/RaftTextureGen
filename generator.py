e0993f06d635b7edc1f774e5a557547b7bb8022a
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-02-05 15:42:56 -0800

    detail

--- generator.py
+++ generator.py
@@ -7,14 +7,24 @@ width, height = 128, 128  # Larger size for the raft sprite
 # Define colors
 bamboo_color = (34, 139, 34)  # Green for bamboo
 lash_color = (139, 69, 19)    # Brown for lashing
+bamboo_shadow_color = (0, 100, 0)  # Darker green for bamboo detail
+lash_shadow_color = (101, 51, 0)   # Darker brown for lashing detail
 
 def draw_bamboo_plank(draw, x, y, plank_width, plank_height):
-    # Draw a single bamboo plank
+    # Draw a single bamboo plank with some texture
     draw.rectangle([x, y, x + plank_width, y + plank_height], fill=bamboo_color, outline="black")
+    # Add texture lines to the bamboo plank
+    for i in range(5):
+        line_x = random.randint(x, x + plank_width)
+        draw.line([line_x, y, line_x, y + plank_height], fill=bamboo_shadow_color, width=1)
 
 def draw_lashing(draw, x, y, lash_width, lash_height):
-    # Draw lashing
+    # Draw lashing with some texture
     draw.rectangle([x, y, x + lash_width, y + lash_height], fill=lash_color, outline="black")
+    # Add texture lines to the lashing
+    for i in range(2):
+        line_y = random.randint(y, y + lash_height)
+        draw.line([x, line_y, x + lash_width, line_y], fill=lash_shadow_color, width=1)
 
 def generate_raft_image():
     # Create an image with a white background
@@ -22,22 +32,38 @@ def generate_raft_image():
     draw = ImageDraw.Draw(image)
 
     # Define plank and lashing sizes
-    plank_width = width // 4
-    plank_height = height - 20
-    lash_width = 6
-    lash_height = 10
+    plank_width = width // 5
+    plank_height = height - 40
+    lash_width = 8
+    lash_height = 15
 
     # Draw bamboo planks
-    for i in range(4):
+    for i in range(5):
         x = i * plank_width + 10
-        y = 10
+        y = 20
         draw_bamboo_plank(draw, x, y, plank_width - 10, plank_height)
 
-    # Draw lashings
-    for i in range(5):
+    # Draw lashings around the raft
+    for i in range(6):
+        # Top lashings
         x = i * plank_width - lash_width // 2 + 10
-        y = height // 2 - lash_height // 2
+        y = 10
         draw_lashing(draw, x, y, lash_width, lash_height)
+
+        # Bottom lashings
+        x = i * plank_width - lash_width // 2 + 10
+        y = height - 25
+        draw_lashing(draw, x, y, lash_width, lash_height)
+
+    # Draw vertical lashings (left and right sides)
+    for i in range(2):
+        x = 5
+        y = i * (plank_height // 2) + 20
+        draw_lashing(draw, x, y, lash_height, lash_width)
+
+        x = width - 25
+        y = i * (plank_height // 2) + 20
+        draw_lashing(draw, x, y, lash_height, lash_width)
 
     return image
 
